b81e109f5de03f046da79d4d8b1ce714479266ae
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-11 18:47:09 +0300

    Added SimpleLightbox lib, added gallery02.js script

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ description, original, preview }) => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+}).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+
+const gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+console.log(gallery);
