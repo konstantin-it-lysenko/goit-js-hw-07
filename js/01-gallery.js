3f35cf9c93da0da02828d3925ed35b02171dbee3
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-09-11 18:46:48 +0300

    Added basicLightbox lib, added gallery01.js script

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ description, original, preview }) => {
+  return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+}).join('');
+
+galleryEl.insertAdjacentHTML('beforeend', markup);
+galleryEl.addEventListener('click', openTargetImgModal);
+
+function openTargetImgModal(e) {
+  e.preventDefault();
+
+  const currentImg = e.target;
+
+  if (currentImg === e.currentTarget) return;
+
+  const imgDataset = currentImg.dataset.source;
+  const imgDescription = currentImg.alt;
+
+  const instance = basicLightbox.create(`
+  <div class="modal">
+  <img src="${imgDataset}" alt="${imgDescription}"/>
+  </div>
+  `, { onClose: () => { document.removeEventListener('keydown', onEscKeydown); } })
+
+  instance.show();
+
+  document.addEventListener('keydown', onEscKeydown);
+
+  function onEscKeydown(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
